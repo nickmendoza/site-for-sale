7f19c1ad2e007477c5403489f7348c4a62fd2d8e
Nick Mendoza <nickmendoza@yahoo.com>
2018-10-12 15:36:21 +0000

    Base files added

---
+++ README.md
@@ -0,0 +1,18 @@
+# Site For Sale Landing Page
+
+### Summary:
+Easily offer all of your sites for sale by displaying a message and listing a contact email address for your primary and parked domains.  It will dynamically display the domain for whichever site was visited in the site's title and on the page itself.  It also dynamically lists the current copyright year, based on the server's date.
+
+![Demonstration Image](https://mobilecodemedia.com/wp-content/uploads/2020/01/site-for-sale.png)
+
+### Instructions:
+Clone the repository and move all of the files into your web root.  This is generally `/var/www/html` or your `public_html` folder:
+
+```shell script
+$ git clone git@github.com:nickmendoza/site-for-sale.git
+```
+
+### License:
+
+- MIT
+- http://mobilecodemedia.com
---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Set html language code.  Default is English.
+ *
+ * @see https://www.w3schools.com/tags/ref_language_codes.asp
+ */
+$language = 'en';
+
+/**
+ * This is how the email address will appear on the page.
+ * You'll probably not want to put your contact email address
+ * in plain text, because it will be scraped and spammed by bots.
+ *
+ * There are a few steps you can take, such as those in the
+ * referenced link:
+ *
+ * @see https://www.smartlabsoftware.com/howto/hide-email-spambots.htm
+ */
+$contactEmail = 'username [at] gmail.com';
+
+/**
+ * Set whatever text is desired to show
+ * on the main "For Sale" page.
+ */
+$contactText = "Please contact {$contactEmail}";
+$domain = trim($_SERVER["HTTP_HOST"]);
+$afterDomainText = ' is for sale.';
+$headTitle = strtoupper($domain) . $afterDomainText;
+$bodyTitle = $domain . $afterDomainText;
+
+/**
+ * Turn on output buffering to make
+ * the page load more quickly.
+ */
+ob_start();
+
+?>
+<!DOCTYPE html>
+<html lang="<?= $language; ?>">
+<head>
+<meta charset="utf-8">
+<title><?= $headTitle; ?></title>
+<link rel="icon" type="image/x-icon" href="/favicons/favicon.ico">
+<style>
+#divMainText {
+  font-size: 30px;
+}
+.text-center {
+  text-align: center;
+}
+</style>
+</head>
+<body>
+
+<div id="divMainText" class="text-center">
+    <h1><?= $bodyTitle; ?></h1>
+    <h2><?= $contactText; ?></h2>
+    <p>&copy; <?= date('Y') . " {$domain}"; ?>.
+        Template provided by <a href="http://mobilecodemedia.com">MobileCodeMedia.com</a>
+    </p>
+</div>
+
+</body>
+</html>
+
+<?php
+
+/**
+ * Output everything that was buffered.
+ */
+ob_end_flush();
